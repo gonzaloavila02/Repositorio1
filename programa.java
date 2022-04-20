8990113e19bea4a7bcc86bdc1b818d6cc5674c5f
gonzaloavila <gonzalocdf123@gmail.com>
2022-04-20 18:37:02 +0200

    Tercer commit + nuevo fichero

--- programa.java
+++ programa.java
@@ -4,6 +4,7 @@ public class programa {
 	public static void main(String[] args) {
 		System.out.println("Primera linea de la prueba");
 		System.out.println("Segunda linea de la prueba");
+		System.out.println("Tercera linea de la prueba");
 
 	}
 
---
+++ programa2.java
@@ -0,0 +1,9 @@
+
+public class programa2 {
+
+	public static void main(String[] args) {
+		System.out.println("Programa 2");
+
+	}
+
+}
